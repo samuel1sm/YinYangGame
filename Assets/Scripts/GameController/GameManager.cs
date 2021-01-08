04d2ea4e909c53a5a7b0e3cad819c1e205116403
samuel <fsamuelsmartins@gmail.com>
2021-01-08 15:22:40 -0300

    Level Menu Donne

---
+++ Assets/Scripts/GameController/GameManager.cs
@@ -0,0 +1,56 @@
+﻿using UnityEngine;
+using UnityEngine.SceneManagement;
+public class GameManager : MonoBehaviour
+{
+    GameManager gameManager;
+    [SerializeField] private int auxScenesQtd = 2;
+    private void Awake()
+    {
+        if (gameManager == null)
+        {
+            gameManager = this;
+            DontDestroyOnLoad(gameManager);
+        }
+        else
+        {
+            Destroy(this);
+        }
+    }
+
+    public void ChangeScene(int sceneNumber)
+    {
+        SceneManager.LoadScene(sceneNumber);
+    }
+
+    public int[] GetLevels()
+    {
+        int sceneQtd = SceneManager.sceneCountInBuildSettings;
+
+        int[] levels = new int[sceneQtd - auxScenesQtd];
+
+        for(int i = auxScenesQtd; i < sceneQtd; i++)
+        {
+            levels[i - auxScenesQtd] = i;
+        }
+
+        return levels;
+    }
+
+    public void QuitGame()
+    {
+        Application.Quit();
+    }
+
+
+    // Start is called before the first frame update
+    void Start()
+    {
+        
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        
+    }
+}
---
+++ Assets/Scripts/GameController/ListLevels.cs
@@ -0,0 +1,44 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using TMPro;
+using UnityEngine.UI;
+
+public class ListLevels : MonoBehaviour
+{
+    [SerializeField] GameObject levelButton;
+    [SerializeField] private float xOffset;
+    [SerializeField] private float yOffset;
+
+    private Transform initial_position;
+    private GameManager gameManager;
+    
+    // Start is called before the first frame update
+    private void Awake()
+    {
+        initial_position = transform.GetChild(0);
+        gameManager = FindObjectOfType<GameManager>();
+        int[] levels = gameManager.GetLevels();
+        int levelsUploaded = 0;
+        foreach(int i in levels)
+        {
+            GameObject newButton = Instantiate(levelButton, initial_position);
+            newButton.transform.localPosition = new Vector2(levelsUploaded * xOffset, levelsUploaded * yOffset);
+            levelsUploaded++;
+            newButton.GetComponentInChildren<TextMeshProUGUI>().text = i.ToString();
+            newButton.GetComponentInChildren<Button>().onClick.AddListener(delegate { gameManager.ChangeScene(i); });
+
+        }
+
+    }
+    void Start()
+    {
+        
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        
+    }
+}
